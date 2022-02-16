1ae1d0246c2085d2c05edfca77fe3d8ba726d9a7
Benroberts37 <benrobsemail@gmail.com>
2022-02-15 20:09:34 -0800

    Finishing assignment

---
+++ src/Components/NasaPhoto.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const dummyData = {
+    date: "2021-12-23",
+    explanation: "some dummy data",
+    hdurl: "https://agod.nasa.goc/apid/image",
+    title: "Three planets and a comet"
+  }
+
+const NasaPhoto = (props) => {
+    return (
+        <div className ="nasa-photo-wrapper">
+        <h3>{props.photo.title}</h3>    
+        <p>{props.photo.data}</p>
+        <img src={props.photo.hdurl} />
+        <p>{props.photo.explanation}</p>
+        </div>
+    )
+}
+
+export default NasaPhoto;
